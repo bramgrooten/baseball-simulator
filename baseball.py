03a3ab8f0a8aa0087f9c9b185f6f5027d5b4a35a
Bram Grooten <b.j.grooten@student.tue.nl>
2022-01-22 14:58:42 +0100

    it works!

--- baseball.py
+++ baseball.py
@@ -41,36 +41,27 @@ class Game:
         self.prob_score_from_2nd_on_single = prob_score_from_2nd_on_single
         self.prob_score_from_1st_on_double = prob_score_from_1st_on_double
 
-
     def reset_game_state(self):
-        self.game_state = {'score': 0, 'inning':    1,
-                           'outs':  0, 'batter_up': 1,
-                           # 'bases': [False, False, False]}
-                           '1st_base': False,
-                           '2nd_base': False,
-                           '3rd_base': False}
+        self.game_state = {'score': 0, 'outs':  0, 'batter_up': 0,
+                           '1st_base': False, '2nd_base': False, '3rd_base': False}
 
     def reset_inning_state(self):
         self.game_state['1st_base'] = False
         self.game_state['2nd_base'] = False
-        self.game_state['3rd_base '] = False
+        self.game_state['3rd_base'] = False
         self.game_state['outs'] = 0
 
     def play(self):
-        if self.printing:
-            self.print_lineup()
         for inning in range(self.nr_innings):
             if self.printing:
                 print(f"\nInning {inning+1}")
             self.play_inning()
-            self.game_state['inning'] += 1
 
     def play_inning(self):
         self.reset_inning_state()
         while self.game_state['outs'] < 3:
             self.steal()
             self.play_batter()
-            self.double_play()
             self.next_batter()
 
     def play_batter(self):
@@ -104,7 +95,7 @@ class Game:
             print("Ball in play, out.")
         self.game_state['outs'] += 1
         runners = [self.game_state['1st_base'], self.game_state['2nd_base'], self.game_state['3rd_base']].count(True)
-        if runners > 0:
+        if runners > 0 and self.game_state['outs'] < 3:
             coinflip = np.random.rand()
             if coinflip < self.prob_advance_runner_on_out:
                 self.sac_fly_or_bunt()
@@ -309,48 +300,78 @@ class Game:
                     print("Steal home! Run scored!")
 
     def double_play(self):
-        if self.game_state['outs'] < 3:
-            self.game_state['outs'] += 1
-            if self.printing:
-                print("Double play!")
-            if self.game_state['1st_base'] and self.game_state['2nd_base'] and self.game_state['3rd_base']:
-                # bases loaded, dp over home and 1st
-                self.game_state['1st_base'] = False
-            elif self.game_state['1st_base'] and self.game_state['2nd_base'] and not self.game_state['3rd_base']:
-                # 1st and 2nd, dp over 2nd and 1st
-                self.game_state['1st_base'] = False
-                self.game_state['2nd_base'] = False
-                self.game_state['3rd_base'] = True
-            elif self.game_state['1st_base'] and not self.game_state['2nd_base'] and self.game_state['3rd_base']:
-                # 1st and 3rd, dp over 2nd and 1st
-                if self.game_state['outs'] < 3:
-                    self.game_state['score'] += 1
-                self.game_state['1st_base'] = False
-                self.game_state['3rd_base'] = False
-            elif self.game_state['1st_base'] and not self.game_state['2nd_base'] and not self.game_state['3rd_base']:
-                # 1st only, std dp
-                self.game_state['1st_base'] = False
-            elif not self.game_state['1st_base'] and self.game_state['2nd_base'] and self.game_state['3rd_base']:
-                # 2nd and 3rd, sac fly out at home
-                self.game_state['3rd_base'] = False
-            elif not self.game_state['1st_base'] and self.game_state['2nd_base'] and not self.game_state['3rd_base']:
-                # 2nd only, sac fly out at 3rd
-                self.game_state['2nd_base'] = False
-            elif not self.game_state['1st_base'] and not self.game_state['2nd_base'] and self.game_state['3rd_base']:
-                # 3rd only, sac fly out at home
-                self.game_state['3rd_base'] = False
-            # else, bases empty, dp not possible
+        self.game_state['outs'] += 1
+        if self.printing:
+            print("Double play!")
+        if self.game_state['1st_base'] and self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # bases loaded, dp over home and 1st
+            self.game_state['1st_base'] = False
+        elif self.game_state['1st_base'] and self.game_state['2nd_base'] and not self.game_state['3rd_base']:
+            # 1st and 2nd, dp over 2nd and 1st
+            self.game_state['1st_base'] = False
+            self.game_state['2nd_base'] = False
+            self.game_state['3rd_base'] = True
+        elif self.game_state['1st_base'] and not self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # 1st and 3rd, dp over 2nd and 1st
+            if self.game_state['outs'] < 3:
+                self.game_state['score'] += 1
+            self.game_state['1st_base'] = False
+            self.game_state['3rd_base'] = False
+        elif self.game_state['1st_base'] and not self.game_state['2nd_base'] and not self.game_state['3rd_base']:
+            # 1st only, std dp
+            self.game_state['1st_base'] = False
+        elif not self.game_state['1st_base'] and self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # 2nd and 3rd, sac fly out at home
+            self.game_state['3rd_base'] = False
+        elif not self.game_state['1st_base'] and self.game_state['2nd_base'] and not self.game_state['3rd_base']:
+            # 2nd only, sac fly out at 3rd
+            self.game_state['2nd_base'] = False
+        elif not self.game_state['1st_base'] and not self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # 3rd only, sac fly out at home
+            self.game_state['3rd_base'] = False
+        # else, bases empty, dp not possible
 
     def sac_fly_or_bunt(self):
-        # todo
-        # runners might advance even if batter is out
-        pass
+        if self.printing:
+            print("Runner(s) advance(s) on the play.")
+        if self.game_state['1st_base'] and self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # bases loaded, all runners move up
+            self.game_state['score'] += 1
+            self.game_state['1st_base'] = False
+        elif self.game_state['1st_base'] and self.game_state['2nd_base'] and not self.game_state['3rd_base']:
+            # 1st and 2nd, both move up
+            self.game_state['1st_base'] = False
+            self.game_state['3rd_base'] = True
+        elif self.game_state['1st_base'] and not self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # 1st and 3rd, both move up
+            self.game_state['score'] += 1
+            self.game_state['1st_base'] = False
+            self.game_state['2nd_base'] = True
+            self.game_state['3rd_base'] = False
+        elif self.game_state['1st_base'] and not self.game_state['2nd_base'] and not self.game_state['3rd_base']:
+            # 1st only, moves up
+            self.game_state['1st_base'] = False
+            self.game_state['2nd_base'] = True
+        elif not self.game_state['1st_base'] and self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # 2nd and 3rd, both move up
+            self.game_state['score'] += 1
+            self.game_state['2nd_base'] = False
+        elif not self.game_state['1st_base'] and self.game_state['2nd_base'] and not self.game_state['3rd_base']:
+            # 2nd only, moves up
+            self.game_state['2nd_base'] = False
+            self.game_state['3rd_base'] = True
+        elif not self.game_state['1st_base'] and not self.game_state['2nd_base'] and self.game_state['3rd_base']:
+            # 3rd only, scores
+            self.game_state['score'] += 1
+            self.game_state['3rd_base'] = False
+        # else, bases empty, advancing runners not possible
 
     def next_batter(self):
-        if self.game_state['batter_up'] < 9:
+        # batter indices go from 0 to 8
+        if self.game_state['batter_up'] < 8:
             self.game_state['batter_up'] += 1
         else:
-            self.game_state['batter_up'] = 1
+            self.game_state['batter_up'] = 0
 
     def print_lineup(self):
         print("The lineup for today's game is:\n")
@@ -394,16 +415,18 @@ if __name__ == '__main__':
         batter = Batter(player_id=batter_id, dataset=dataset)
         lineup.append(batter)
 
-    # game = Game(lineup)
-    # game.print_lineup()
-    # scores = []
-    # nr_games = 100
-    # for _ in range(nr_games):
-    #     game.reset_game_state()
-    #     game.play()
-    #     scores.append(game.get_score())
-    # avg_score = np.mean(scores)
-    #
-    # print(f"\nAverage score after {nr_games} games is: {avg_score} runs.\n")
+    game = Game(lineup, printing=False)
+    game.print_lineup()
+    scores = []
+    nr_games = 100
+    for _ in range(nr_games):
+        game.reset_game_state()
+        game.play()
+        scores.append(game.get_score())
+
+    print(f"\nAfter {nr_games} games we have:")
+    print(f"avg score: \t{np.mean(scores)}")
+    print(f"median: \t{np.median(scores)}")
+    print(f"std dev: \t{np.std(scores):.2f}")
 
 
