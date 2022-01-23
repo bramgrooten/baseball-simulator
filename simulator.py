dbb5c68cead123952b291762e0bb183cda634942
Bram Grooten <b.j.grooten@student.tue.nl>
2022-01-23 12:44:07 +0100

    add own lineup with probs

--- simulator.py
+++ simulator.py
@@ -11,7 +11,7 @@ import numpy as np
 import plotille
 
 
-def setup():
+def setup_mlb():
     dataset = pd.read_excel("mlb2019.xls", sheet_name=0, header=0)
     # marteke01
     # lemahdj01
@@ -38,6 +38,24 @@ def setup():
     for batter_id in lineup_id:
         batter = Batter(player_id=batter_id, dataset=dataset)
         lineup.append(batter)
+    return lineup
+
+
+def setup_own():
+    lineup = [
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage1'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage2'),
+        Batter(probabilities=[0.150, 0.400, 0.150, 0.150, 0.045, 0.005, 0.100], name='MikeHomerun'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage4'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage5'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage6'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage7'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage8'),
+        Batter(probabilities=[0.150, 0.500, 0.150, 0.150, 0.045, 0.005, 0.000], name='JoeAverage9'),
+        #                      K    out     walk   1B      2B     3B    HR
+    ]
+    # Anthony Rendon:
+    # strike-out: 0.133  in-play-out: 0.474  walk: 0.124  single: 0.144  double: 0.068	 triple: 0.005 	homerun: 0.053
     return lineup
 
 
@@ -72,8 +90,10 @@ def play_one_game(lineup):
 
 
 if __name__ == '__main__':
-    lineup = setup()
+    lineup = setup_mlb()
+    # lineup = setup_own()
 
-    play_many_games(lineup, nr_games=10_000)
-    # play_one_game(lineup)
+    # play_many_games(lineup, nr_games=10_000)
+    play_one_game(lineup)
+
 
